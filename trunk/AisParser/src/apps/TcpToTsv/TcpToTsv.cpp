f2b2c8b6da49adb6b244ce625663fdb957ebf671
bryan@midnightsignal.com <bryan@midnightsignal.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2012-03-31 00:18:15 +0000

    new input argument to break up the output TSV into chunks decided by the user

--- trunk/AisParser/src/apps/TcpToTsv/TcpToTsv.cpp
+++ trunk/AisParser/src/apps/TcpToTsv/TcpToTsv.cpp
@@ -4,12 +4,9 @@
 #include <boost/timer/timer.hpp>
 
 //Input sources
-//#include <AisFlatFileInputSource.h>
 #include <AisTcpStreamInputSource.h>
 
 //Output sources
-//#include <AisDatabaseWriter.h>
-//#include <AisCsvWriter.h>
 #include <AisTsvWriter.h>
 
 //Parsers
@@ -24,7 +21,9 @@ using namespace std;
 
 void usage()
 {
-	cerr << "AisParserApp.exe <hostname> <port>" << endl;
+	cerr << "AisParserApp.exe <hostname> <port> <number-of-entries-per-tsv>" << endl;
+	cerr << "Will create a file named <input-filename>.p<partition-number>.tsv with the parsed AIS.\nThis file will be suitable for uploading using SQL Loader." << endl;
+	cerr << "<number-of-entries-per-tsv> is the number of entries per file.\nIt will create mutliple files named <input-filename><file-number>.tsv.\nIf set to 0, it will push all to a single file." << endl;
 }
 
 //========================================================================================
@@ -36,7 +35,7 @@ int main(int argc, char** argv)
 {
 
 	//parse args
-	if(argc!=3)
+	if(argc!=4)
 	{
 		usage();
 		return -1;
@@ -46,74 +45,94 @@ int main(int argc, char** argv)
 
 	std::string host = argv[1];
 	std::string port = argv[2];
+	
+	unsigned int messagesPerFile = 0;
+	try
+	{
+		messagesPerFile = boost::lexical_cast<unsigned int>(argv[3]);
+	}
+	catch(exception& e)
+	{
+		cerr << e.what() << endl;
+		cerr << "Setting messages per file to 0" << endl;
+		messagesPerFile = 0;
+	}
 
 	//Define input class (an AisInputSource)
 	//STEPX: choose the correct type of input source
 	AisTcpStreamInputSource aisInputSource(host, port);
 	
-	//Define output class (an AisWriter)
-	//STEPX: choose the correct type of output source
-	AisTsvWriter aisWriter(host+"port"+port+".tsv");
-
-	if(!aisWriter.isReady())
-	{
-		aisDebug("AisWriter is not ready");
-		return -1;
-	}
+	unsigned int partition = 0;
 
 	while(aisInputSource.isReady())
 	{
-		//load the next sentence from the AIS input to the parser
-		//STEPX: choose the correct type of sentence parser
-		AisMsisSentenceParser aisSentenceParser(aisInputSource.getNextSentence());
-		AisMessageParser aisMessageParser;
 
-		if(aisSentenceParser.isMessageValid())
+		//Define output class (an AisWriter)
+		//STEPX: choose the correct type of output source
+		AisTsvWriter aisWriter(host+"port"+port+".p" + boost::lexical_cast<string>(partition++) + ".tsv");
+
+		if(!aisWriter.isReady())
 		{
-			//This check is to make sure that if the first sentence of the message
-			//was bad we won't read the second sentence and parse it as a new message
-			if(aisSentenceParser.getSentenceNumber()==1)
+			aisDebug("AisWriter is not ready");
+			return -1;
+		}
+
+		for(unsigned int messageCount = 0; ((messagesPerFile == 0) || (messageCount < messagesPerFile)) && (aisInputSource.isReady()); messageCount++)
+		{
+
+			//load the next sentence from the AIS input to the parser
+			//STEPX: choose the correct type of sentence parser
+			//AisMsisSentenceParser aisSentenceParser(aisInputSource.getNextSentence());
+			AisSatSentenceParser aisSentenceParser(aisInputSource.getNextSentence());
+			AisMessageParser aisMessageParser;
+
+			if(aisSentenceParser.isMessageValid())
 			{
-				aisMessageParser.addData(aisSentenceParser.getData());	
-				//if the current sentence is part of a multipart message
-				//grab the next message until you have them all, or message is invalid
-				try
+				//This check is to make sure that if the first sentence of the message
+				//was bad we won't read the second sentence and parse it as a new message
+				if(aisSentenceParser.getSentenceNumber()==1)
 				{
-					while(aisSentenceParser.getSentenceNumber() < aisSentenceParser.getNumberOfSentences())
+					aisMessageParser.addData(aisSentenceParser.getData());	
+					//if the current sentence is part of a multipart message
+					//grab the next message until you have them all, or message is invalid
+					try
 					{
-						aisSentenceParser.setSentence(aisInputSource.getNextSentence());
-						if(aisSentenceParser.isMessageValid()){
-							aisMessageParser.addData(aisSentenceParser.getData());	
-						}
-						else
+						while(aisSentenceParser.getSentenceNumber() < aisSentenceParser.getNumberOfSentences())
 						{
-							//aisDebug("Invalid multipart message:\n" + aisSentenceParser.getCurrentSentence());
-							throw exception("Invalid multipart message");
+							aisSentenceParser.setSentence(aisInputSource.getNextSentence());
+							if(aisSentenceParser.isMessageValid()){
+								aisMessageParser.addData(aisSentenceParser.getData());	
+							}
+							else
+							{
+								//aisDebug("Invalid multipart message:\n" + aisSentenceParser.getCurrentSentence());
+								throw exception("Invalid multipart message");
+							}
 						}
+
+						AisMessage aisMessage = aisMessageParser.parseMessage();
+						//add time from ais sentence to the ais message
+						aisMessage.setDATETIME(aisSentenceParser.getTimestamp());
+						//add streamid from ais sentence to the ais message
+						aisMessage.setSTREAMID(aisSentenceParser.getStreamId());
+
+						aisWriter.writeEntry(aisMessage);
 					}
-
-					AisMessage aisMessage = aisMessageParser.parseMessage();
-					//add time from ais sentence to the ais message
-					aisMessage.setDATETIME(aisSentenceParser.getTimestamp());
-					//add streamid from ais sentence to the ais message
-					aisMessage.setSTREAMID(aisSentenceParser.getStreamId());
-
-					aisWriter.writeEntry(aisMessage);
+					catch(exception &e)
+					{
+						cerr << e.what() << endl;
+					}
 				}
-				catch(exception &e)
+				else
 				{
-					cerr << e.what() << endl;
+					aisDebug("First sentence of message was invalid/not receieved.\nSkipping the rest of the sentences of this message");
+					continue;
 				}
 			}
 			else
 			{
-				aisDebug("First sentence of message was invalid/not receieved.\nSkipping the rest of the sentences of this message");
-				continue;
+				//aisDebug("Invalid message:\n" + aisSentenceParser.getCurrentSentence());
 			}
-		}
-		else
-		{
-			//aisDebug("Invalid message:\n" + aisSentenceParser.getCurrentSentence());
 		}
 	}
 	return 0;
