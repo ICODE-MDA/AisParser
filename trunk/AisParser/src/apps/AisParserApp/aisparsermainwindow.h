dbdba8935b176188d30d318bf2fab9a07ff74184
bryan@midnightsignal.com <bryan@midnightsignal.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2012-05-14 20:47:53 +0000

    QT4 gui for Ais Parser

---
+++ trunk/AisParser/src/apps/AisParserApp/aisparsermainwindow.h
@@ -0,0 +1,61 @@
+#ifndef AISPARSERMAINWINDOW_H
+#define AISPARSERMAINWINDOW_H
+
+#include <QMainWindow>
+
+//Input sources
+//#include <AisInputSource.h>
+
+//Output sources
+//#include <AisWriter.h>
+
+#include <AisDebug.h>
+
+using namespace std;
+
+
+namespace Ui {
+class AisParserMainWindow;
+}
+
+class AisParserMainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit AisParserMainWindow(QWidget *parent = 0);
+    ~AisParserMainWindow();
+protected slots:
+    void manageInputVisibility(QString inputChoice);
+    void manageOutputVisibility(QString outputChoice);
+    void directoryChooser();
+    void fileChooser();
+    void startParsingAis();
+    void stopParsingAis();
+    void displayLicense();
+private:
+    //int foo(AisInputSource* input, AisWriter* output);
+    void updateProgress(unsigned int progress, QString message);
+    template<class InputType, class OutputType, class AisSentenceParserType>
+    int pointParser();
+
+    template<class InputType, class OutputType, class AisSentenceParserType>
+    int databaseParser();
+
+    template<class InputType, class OutputType, class AisSentenceParserType>
+    int trackParser();
+
+    bool validateArguments();
+    bool validateDatabaseArguments();
+    bool validateFlatfileOutputArguments();
+    bool validateFlatfileInputArguments();
+    bool validateTcpInputArguments();
+
+    void enableDatabaseFrame(bool enable);
+    void enableTcpPortEntry(bool enable);
+    bool m_continueParsing;
+
+    Ui::AisParserMainWindow *ui;
+};
+
+#endif // AISPARSERMAINWINDOW_H
