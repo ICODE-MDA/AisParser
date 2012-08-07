146e4bffeae8858b9dd8e83f6d3a0986a5b0aa40
bryan@midnightsignal.com <bbagnall@users.noreply.github.com>
2012-08-07 21:50:15 +0000

    added new variables for postgreSQL libs and headers

--- build/build_project.bat
+++ build/build_project.bat
@@ -12,19 +12,23 @@ set oracle_includes="D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\include
 set oracle_library_dir="D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\lib\msvc"
 set mysql_includes="D:\libraries\mysql\v110\include"
 set mysql_library_dir="D:\libraries\mysql\v110\lib"
+set postgresql_includes="D:\libraries\libpqxx\v4_0\include"
+set postgresql_library_dir="D:\libraries\libpqxx\v4_0\lib\x64"
 set icode_third_party_dir="D:\libraries\icode\3rd-party"
 set oracle_bin_dir=D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\bin
 set mysql_bin_dir=D:\libraries\mysql\v110\lib
 
-cmake -G "Visual Studio 10"^
+cmake -G "Visual Studio 10 Win64"^
  -DAIS_PARSER_SRC_DIR=%ais_parser_src_dir%^
  -DICODE_THIRD_PARTY_DIR=%icode_third_party_dir%^
  -DORACLE_INCLUDE_DIR=%oracle_includes%^
  -DORACLE_LIBRARY_PATH=%oracle_library_dir%^
  -DMYSQL_INCLUDE_DIR=%mysql_includes%^
  -DMYSQL_LIBRARY_PATH=%mysql_library_dir%^
+ -DPOSTGRESQL_INCLUDE_DIR=%postgresql_includes%^
+ -DPOSTGRESQL_LIBRARY_PATH=%postgresql_library_dir%^
  -DBOOST_INCLUDE_DIR=%boost_dir%^
- -DBOOST_LIBRARY_PATH=%boost_dir%/stage/lib^
+ -DBOOST_LIBRARY_PATH=%boost_dir%/stage/lib/x64^
  ../src/
 
 
