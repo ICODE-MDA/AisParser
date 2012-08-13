48b4a6948ffdc10471e5725d7681c3059c7f129a
bryan@midnightsignal.com <bbagnall@users.noreply.github.com>
2012-08-13 23:57:46 +0000

    Made CMake scripts compatible with building the QT4 application.
    Added more options to CMake scripts to make them more user friendly. We can now build this application using cmake-gui.
    
    Removed an extra file that was in the AisParserApp directory

--- build/build_project.bat
+++ build/build_project.bat
@@ -5,20 +5,27 @@ set OLDPATH=%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 set PATH=C:\Program Files (x86)\CMake 2.8\bin;%PATH%
+set qt_dir=D:\libraries\qt\qt-everywhere-opensource-src-4.8.2_x86
 
 set boost_dir="D:\libraries\boost\v1_48_0"
 set ais_parser_src_dir="D:\libraries\icode\google-icode-mda\trunk\AisParser\src"
+
 set oracle_includes="D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\include"
 set oracle_library_dir="D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\lib\msvc"
+
 set mysql_includes="D:\libraries\mysql\v110\include"
 set mysql_library_dir="D:\libraries\mysql\v110\lib"
+
 set postgresql_includes="D:\libraries\libpqxx\v4_0\include"
-set postgresql_library_dir="D:\libraries\libpqxx\v4_0\lib\x64"
+set postgresql_library_dir="D:\libraries\libpqxx\v4_0\lib\x86"
+
 set icode_third_party_dir="D:\libraries\icode\3rd-party"
 set oracle_bin_dir=D:\libraries\oracle\instantclient-sdk-nt-11.2.0.2.0\bin
 set mysql_bin_dir=D:\libraries\mysql\v110\lib
 
-cmake -G "Visual Studio 10 Win64"^
+cmake -G "Visual Studio 10"^
+ -DBUILD_ALL_APPS=ON^
+ -DQT_DIR=%qt_dir%^
  -DAIS_PARSER_SRC_DIR=%ais_parser_src_dir%^
  -DICODE_THIRD_PARTY_DIR=%icode_third_party_dir%^
  -DORACLE_INCLUDE_DIR=%oracle_includes%^
@@ -28,7 +35,7 @@ cmake -G "Visual Studio 10 Win64"^
  -DPOSTGRESQL_INCLUDE_DIR=%postgresql_includes%^
  -DPOSTGRESQL_LIBRARY_PATH=%postgresql_library_dir%^
  -DBOOST_INCLUDE_DIR=%boost_dir%^
- -DBOOST_LIBRARY_PATH=%boost_dir%/stage/lib/x64^
+ -DBOOST_LIBRARY_PATH=%boost_dir%/stage/lib/x86^
  ../src/
 
 
