df531d491d653851967c6f5c4c12fa4f633345d4
mdalynne@gmail.com <mdalynne@gmail.com@fdd52888-2fd2-40d9-2659-c92be34f9d9a>
2013-04-11 21:01:47 +0000

    Added capability to write AIS tracks for a specific region and vessel types.  User specifies the following addition command line arguments: lower left latitude (dec deg), upper right latitude (dec deg), lower left longitude (dec deg), upper right longitude (dec deg), vessel type minimum, and vessel type maximum.

--- trunk/AisParser/src/apps/LogToKmlTracks/LogToKmlTracks.cpp
+++ trunk/AisParser/src/apps/LogToKmlTracks/LogToKmlTracks.cpp
@@ -13,13 +13,16 @@
 int main(int argc, char** argv)
 {
 	//parse args
-	if(argc!=3)
+	if(argc < 3 || argc > 9)
 	{
 		trackParserUsage();
 		return -1;
 	}
-
+	
 	string filename = argv[1];
+	cout << "filename " << filename << endl;
+	cout << "argv[2] " << argv[2] << endl;
+	
 	unsigned int tracksPerFile = 0;
 	try
 	{
@@ -31,11 +34,37 @@ int main(int argc, char** argv)
 		cerr << "Setting messages per file to 0" << endl;
 		tracksPerFile = 0;
 	}
-	
+	double urLat, llLat, llLon, urLon;
+	int vesselTypeMin, vesselTypeMax;
+	if (argc > 3)
+	{
+		
+		llLat = atof(argv[3]);
+		cout << "llLat " << llLat << endl;
+		urLat = atof(argv[4]);
+		cout << "urLat " << urLat << endl;
+		llLon = atof(argv[5]);
+		cout << "llLon " << llLon << endl;
+		urLon = atof(argv[6]);
+		cout << "urLon " << urLon << endl;
+		
+		vesselTypeMin = atoi(argv[7]);
+		vesselTypeMax = atoi(argv[8]);
+
+		cout << "vesselTypeMin " << vesselTypeMin << " vesselTypeMax" << vesselTypeMax << endl;
+	}
 	//Define input class (an AisInputSource)
 	//STEPX: choose the correct type of input source
 	AisFlatFileInputSource aisInputSource(filename);
-
-	trackParser<AisKmlTrackWriter, AisSatSentenceParser>(aisInputSource, filename, tracksPerFile);
+	if (argc == 3) 
+	{
+		trackParser<AisKmlTrackWriter, AisSatSentenceParser>(aisInputSource, filename, tracksPerFile);
+	} 
+	else
+	{
+		// Limit track parser by geographic area and vessel type
+		cout << "before track parser limited " << endl;
+		trackParserLimited<AisKmlTrackWriter, AisSatSentenceParser>(aisInputSource, filename, tracksPerFile, urLat, llLat, urLon, llLon, vesselTypeMin, vesselTypeMax);
+	}
 	return 0;
 }
